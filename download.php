d00c8cebbafd20200e7ef03163553bcd3ae1019a
Navneet Singh Chouhan <chouhan.navneet1986@gmail.com>
2023-09-10 07:48:28 +0530

    Fix...

--- download.php
+++ download.php
@@ -1,2 +1,12 @@
 <?php
 $imageFile = __DIR__ . '/output/' . $_GET['genFile'];
+
+// Set the appropriate headers for a file download
+header('Content-Type: application/octet-stream');
+header('Content-Disposition: attachment; filename="' . basename($imageFile) . '"');
+
+
+header('Content-Length: ' . filesize($imageFile));
+
+// Output the image file contents
+readfile($imageFile);
